ea6c85fa8abe027a7e14787de20b9d2a817ae4b1
Alex Tang <tang.alex.93@gmail.com>
2015-10-17 14:30:09 -0700

    quick fix

--- www/js/habit-all/habit-all-ctrl.js
+++ www/js/habit-all/habit-all-ctrl.js
@@ -1,7 +1,50 @@
-angular.module('tracktr.controllers', ['tasks'] ) // 'tasks' is causing error
+angular.module('tracktr.controllers') // 'tasks' is causing error
 
 .controller("HabitAllController", function($scope, $state) {
   
+
+  var allTasks = [{
+      id: 0,
+      name: 'Daily Everyday Not Active',
+      isActive: false,
+      frequency: 0, //daily
+      days: [0,1,2,3,4,5,6,7],
+      goal: 5,
+      record: [5]
+    }, {
+      id: 1,
+      name: 'Daily Active Some days',
+      isActive: true,
+      frequency: 0, //daily
+      days: [0,1,3],
+      goal: 5,
+      record: [5]
+    }, {
+          id: 2,
+      name: 'Weekly Active Some days',
+      isActive: true,
+      frequency: 1, //weekly
+      days: [0,1,3],
+      goal: 5,
+      record: [5,5]
+    }, {
+      id: 2,
+      name: 'monthly Active Some days',
+      isActive: true,
+      frequency: 2, //monthly
+      days: [2,4],
+      goal: 5,
+      record: [5]
+    }
+  ];  
+
+
+
+  this.tasks = allTasks;
+  
+  $scope.items = allTasks;
+
+
   /**
    * Click handler for new habit button
    */
@@ -10,53 +53,4 @@ angular.module('tracktr.controllers', ['tasks'] ) // 'tasks' is causing error
   }
 	
 });
-
-(function(){
-  
-var app = angular.module('tasks',[]);
-
-app.controller('TasksAllController', function($scope){
-  
-  this.tasks = allTasks;
-  
-  $scope.items = allTasks;
-  
-});
-
-var allTasks = [{
-    id: 0,
-    name: 'Daily Everyday Not Active',
-    isActive: false,
-    frequency: 0, //daily
-    days: [0,1,2,3,4,5,6,7],
-    goal: 5,
-    record: [5]
-  }, {
-    id: 1,
-    name: 'Daily Active Some days',
-    isActive: true,
-    frequency: 0, //daily
-    days: [0,1,3],
-    goal: 5,
-    record: [5]
-  }, {
-        id: 2,
-    name: 'Weekly Active Some days',
-    isActive: true,
-    frequency: 1, //weekly
-    days: [0,1,3],
-    goal: 5,
-    record: [5,5]
-  }, {
-    id: 2,
-    name: 'monthly Active Some days',
-    isActive: true,
-    frequency: 2, //monthly
-    days: [2,4],
-    goal: 5,
-    record: [5]
-  }
-  ];  
-  
-})();
 
