de640940a552f7823459702e02500f273321c28b
Edison Yang <edison.cy.yang@gmail.com>
2015-10-28 13:08:35 -0700

    Resolve merge conflict

--- www/js/views/home/home-ctrl.js
+++ www/js/views/home/home-ctrl.js
@@ -2,58 +2,6 @@ angular.module('tracktr.controllers', [])
 
 .controller('HomeController', function($scope, $state, TaskService) {
   
-// <<<<<<< HEAD
-  
-//   var emptyArray = [];
-// 	var currentDate = new Date();
-// 	// var aDays = { id:1, sunday: 0, monday: 1, tuesday: 1, wednesday: 1, 
-// 	// 	thursday: 0, friday: 1, saturday: 0 };
-// 	// var tasks = 
-//   // [
-//   //   { 
-//   //    id: 1,
-//   //    name: "Read for 5 minutes",
-//   //    isActive: true,
-//   //    frequency: 0, 
-//   //    isCount: 1,
-//   //    isTime: 0,
-//   //    goal: 5,
-//   //    icon: 0, 
-//   //    count:0,
-//   //    isTimerRunning: false,
-//   //    creationDate: currentDate,
-//   //    progress: [{
-//   //       id: 1,
-//   //       task_id: 1,
-//   //       date: currentDate,
-//   //       progress: 0,
-//   //       timerLastStarted: currentDate
-//   //     }],
-// 	// 	 days: { id:1, sunday: 0, monday: 1, tuesday: 1, wednesday: 1, 
-// 	// 	thursday: 0, friday: 1, saturday: 0 }
-//   //   },
-//   //   { id:2,
-//   //     name: "Watch a Cosmos episode",
-//   //     isActive: true, 
-//   //     frequency: 0,
-//   //     isCount: 1,
-//   //     isTime: 0,
-//   //     goal: 60,
-//   //     icon: 0,
-//   //     count:0,
-//   //     isTimerRunning: false,
-//   //     creationDate: currentDate,
-//   //     progress: [{
-//   //       id: 2,
-//   //       task_id: 2,
-//   //       date: currentDate,
-//   //       progress: 5,
-//   //       timerLastStarted: currentDate
-//   //     }],
-// 	// 	  days: { id:1, sunday: 0, monday: 1, tuesday: 1, wednesday: 1, 
-// 	// 	thursday: 0, friday: 1, saturday: 0 } 
-//   //   }
-//   // ];
   
   $scope.allTasks;
         
@@ -70,15 +18,8 @@ angular.module('tracktr.controllers', [])
    * Increment the count for count tasks
    */
   $scope.incCount = function(task) {
-// <<<<<<< HEAD
-//     if(task.progress.length === 0) {
-//       $scope.startProgress(task);
-//     }
-//     task.progress[0].progress+=1;
-// =======
     $scope.startProgress(task);
     task.progress[task.progress.length - 1].progress+=1;
-// >>>>>>> edison-branch2
     TaskService.updateTask(task, function(err){});
   };
   
@@ -153,14 +94,6 @@ angular.module('tracktr.controllers', [])
   };
  
   
-// <<<<<<< HEAD
-  
-//   /*TODO
-//   * calculate the time passed in timer task
-//   */
-//   $scope.processTimer = function() {
-//     var current_date = new Date();
-// =======
   /*
    * Count the current progress, and express it in seconds
    */
@@ -252,15 +185,6 @@ angular.module('tracktr.controllers', [])
        TaskService.updateTask(task);
        mytimeout = $timeout($scope.onTimeout, 1000);
     };
- 
- 
-    // stops and resets the current timer
-// <<<<<<< HEAD
-//     $scope.stopTimer = function() {
-//         $scope.$broadcast('timer-stopped', $scope.counter);
-//         $scope.seconds = 0;
-//         $scope.minutes = 0;
-//         $scope.counter = 0;
         
 
     $scope.stopTimer = function(task) {
