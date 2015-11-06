6f52ebda397a7620a5c01c78f9b2470699963ccc
Edison Yang <edison.cy.yang@gmail.com>
2015-11-06 02:17:59 -0800

    Plot monthly progress chart (for November only)

--- www/js/views/habit-charts/habit-charts-ctrl.js
+++ www/js/views/habit-charts/habit-charts-ctrl.js
@@ -6,6 +6,9 @@ angular.module('tracktr.controllers')
 	console.log($scope.taskId);
 	$scope.labels = ['Nov. 1', 'Nov. 2', 'Nov. 3', 'Nov. 4', 'Nov. 5', 'Nov. 6', 'Nov. 7','Nov. 8','Nov. 9','Nov. 10','Nov. 11','Nov. 12','Nov. 13'];
   $scope.series = ['Series A', 'Series B'];
+  
+  //Month names
+  $scope.monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
@@ -17,12 +20,34 @@ angular.module('tracktr.controllers')
   
   $scope.loadProgress = function() {
     TaskService.getTaskById($scope.taskId, function(err, task) { 
-      $scope.task = task;
+    $scope.task = task;
       
-      for(var i = 0; i < task.progress.length; i++) {
-        $scope.progress[0][i] = task.progress[i].progress;
-        console.log("progress " + i + " is " + $scope.progress[0][i]);
-      }
+      //Only loads every progress entry into progress, to display on chart
+      // for(var i = 0; i < task.progress.length; i++) {
+      //   $scope.progress[0][i] = task.progress[i].progress;
+      //   console.log("progress " + i + " is " + $scope.progress[0][i]);
+      // }
+      
+    var today = new Date();
+    var firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(),1);
+    var lastDayOfMonth = new Date(today.getFullYear(), today.getMonth()+1);
+    var result = 0;
+    
+    for(var i = 0; i < $scope.task.progress.length ; i++){
+      
+      if(firstDayOfMonth.getTime() <= $scope.task.progress[i].date.getTime() && $scope.task.progress[i].date.getTime() <= lastDayOfMonth.getTime()){
+         result += $scope.task.progress[i].progress;
+      } 
+    }
+    
+    if(task.isTime) {
+      result = Math.floor(result / 1000);
+    }
+    console.log(result);
+    $scope.progress[0][0] = result;  
+    $scope.labels = [$scope.monthNames[today.getMonth()]];
+    
+      
     });
   };
 });
