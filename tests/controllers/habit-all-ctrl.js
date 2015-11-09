749edb1a06ddc0e71a3646baab9ad3ff5e7a9bfb
someHobo <xwei7778@gmail.com>
2015-11-08 21:14:10 -0800

    Added additional Sunday check to a test where it was a daily task and it was testing to see if it should count progress from last Sunday

--- tests/controllers/habit-all-ctrl.js
+++ tests/controllers/habit-all-ctrl.js
@@ -37,10 +37,21 @@ describe("Habit All Controller Tests", function(){
     });
     
     it("Test count daily progress where date is last Sunday", function(){
-       
+       /**
+       * If today is Sunday. Previous Sunday will equate to previous Sunday from last week.
+       * This means that the progress won't be counted.
+       * 
+       * However, if today is not Sunday. Then progress will be counted, because it's
+       * technically today.
+       */
+      var today = new Date();
       var task = new Task(tasks[14]);
       var result = task.getProgress();
+      if(today.getDay()===0){
       expect(result).toBe(0);
+      }else{
+         expect(result).toBe(10); 
+      }
       
     });
     
