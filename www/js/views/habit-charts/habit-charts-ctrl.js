3a7a9cfc96654e0604824a1463724243570d20af
Edison Yang <edison.cy.yang@gmail.com>
2015-11-09 16:12:08 -0800

    Display daily progress chart

--- www/js/views/habit-charts/habit-charts-ctrl.js
+++ www/js/views/habit-charts/habit-charts-ctrl.js
@@ -4,7 +4,8 @@ angular.module('tracktr.controllers')
 	$scope.testBoolean = true;
   $scope.taskId = $stateParams.taskId;
 	console.log($scope.taskId);
-	$scope.labels = ['Nov. 1', 'Nov. 2', 'Nov. 3', 'Nov. 4', 'Nov. 5', 'Nov. 6', 'Nov. 7','Nov. 8','Nov. 9','Nov. 10','Nov. 11','Nov. 12','Nov. 13'];
+	// $scope.labels = ['Nov. 1', 'Nov. 2', 'Nov. 3', 'Nov. 4', 'Nov. 5', 'Nov. 6', 'Nov. 7','Nov. 8','Nov. 9','Nov. 10','Nov. 11','Nov. 12','Nov. 13'];
+  $scope.labels = [];
   $scope.series = ['Series A', 'Series B'];
   
   //Month names
@@ -54,4 +55,40 @@ angular.module('tracktr.controllers')
   ///pseudo code for displaying daily: go through every progress entry, put first one into a date, and a progressChart
   ///for every entry i, check if the date is the same as i-1, if it is, add the progress to it
   ///if not, append new element to the date (label) array, append new element to data array
+  $scope.loadDailyProgress = function() {
+    TaskService.getTaskById($scope.taskId, function(err, task) { 
+      $scope.task = task;
+      
+      var previousDate = null;
+      var currentDate = null;
+      var previousIndex = null;
+      for(var i = 0; i < $scope.task.progress.length; i++) {
+        if(i === 0) {
+          previousDate = ($scope.task.progress[i].date.getMonth()+1) + "/" + $scope.task.progress[i].date.getDate();
+          console.log("Date: " + previousDate);
+          $scope.labels.push(previousDate);
+          $scope.progress[0][i] = $scope.task.progress[i].progress;
+          previousIndex = i;
+        }
+        else {
+          currentDate = ($scope.task.progress[i].date.getMonth()+1) + "/" + $scope.task.progress[i].date.getDate();
+          console.log("Date: " + currentDate);
+          if(currentDate === previousDate) {
+            $scope.progress[0][previousIndex] += $scope.task.progress[i].progress;
+            console.log("progress: " + $scope.progress[0][previousIndex] + "at index " + previousIndex);
+          }
+          else {
+            $scope.labels.push(currentDate);
+            $scope.progress[0][previousIndex + 1] = $scope.task.progress[i].progress;
+            console.log("second progress: " + $scope.progress[0][previousIndex + 1]);
+            previousIndex += 1;
+            previousDate = currentDate;
+          }
+        }
+        
+      }
+      
+    });
+  };
+  
 });
