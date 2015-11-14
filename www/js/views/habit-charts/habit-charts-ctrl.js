18c768a9a9d525a73f7c57abe56c395bdddaa9cf
Edison Yang <edison.cy.yang@gmail.com>
2015-11-14 00:40:53 -0800

    Implemented switching between daily, weekly, and monthly chart with swipe to navigate

--- www/js/views/habit-charts/habit-charts-ctrl.js
+++ www/js/views/habit-charts/habit-charts-ctrl.js
@@ -75,6 +75,9 @@ angular.module('tracktr.controllers')
       $scope.task = task;
       $scope.labels = ['Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thur.', 'Fri.', 'Sat.'];
       $scope.week = option;
+      $scope.isWeekly = true;
+      $scope.isDaily = false;
+      $scope.isMonthly = false;
       
       var today = new Date();
       
@@ -229,6 +232,10 @@ angular.module('tracktr.controllers')
       $scope.labels = ['0:00','1:00','2:00','3:00','4:00','5:00','6:00','7:00','8:00','9:00','10:00','11:00','12:00',
                        '13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00'];
       $scope.data = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
+      $scope.isWeekly = false;
+      $scope.isDaily = true;
+      $scope.isMonthly = false;
+      
       $scope.days = option;                
       var today = new Date();
       today.setDate(today.getDate() - $scope.days);
@@ -368,6 +375,10 @@ angular.module('tracktr.controllers')
     TaskService.getTaskById($scope.taskId, function(err, task) {
       $scope.task = task;
       $scope.data = [[0,0,0,0,0,0,0,0,0,0,0,0]];
+      $scope.isWeekly = false;
+      $scope.isDaily = false;
+      $scope.isMonthly = true;
+      
       $scope.labels = $scope.monthNames;
       $scope.years = option;
       
@@ -416,6 +427,36 @@ angular.module('tracktr.controllers')
       $scope.data[0][i] = $scope.data[0][i]/60000;
       console.log("hihihihihi time is: " + $scope.data[0][i] + "minutes");
     }
+  };
+  
+  
+  /**
+   * 3 different divs, for daily, weekly, monthly
+   * 3 scope variables: isDaily, isWeekly, isMonthly to identify which view user wants to see
+   * ng-show to check the 3 scope variables
+   * do on-swipe-left and right on the chart, also two buttons for previous/next
+   */
+  $scope.showDaily = function() {
+    $scope.isWeekly = false;
+    $scope.isDaily = true;
+    $scope.isMonthly = false;
+    $scope.loadDailyProgress(0);
+  };
+  
+  
+  $scope.showMonthly = function() {
+    $scope.isWeekly = false;
+    $scope.isDaily = false;
+    $scope.isMonthly = true;
+    $scope.loadMonthlyProgress(0);
+  };
+  
+  
+  $scope.showWeekly = function() {
+    $scope.isWeekly = true;
+    $scope.isDaily = false;
+    $scope.isMonthly = false;
+    $scope.loadWeeklyProgress(0);
   };
   
   /**
