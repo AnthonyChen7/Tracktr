0957d7be7dbd06356a500b66e359d10ae3efc71c
Edison Yang <edison.cy.yang@gmail.com>
2015-11-25 03:43:17 -0800

    New smart tip that shows the day of week a task is most frequently worked on

--- www/js/views/habit-charts/habit-charts-ctrl.js
+++ www/js/views/habit-charts/habit-charts-ctrl.js
@@ -410,6 +410,43 @@ angular.module('tracktr.controllers')
   
   
   /**
+   * Return the day of week that the task is most frequently "worked on"
+   */ 
+  $scope.mostFrequentDays = function() {
+    //Only look at weekly or monthly tasks
+    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    if($scope.task.frequency === 1 || $scope.task.frequency === 2) {
+      //Create an array to hold number of times progress happens for each day of the week
+      var daysProgress = [0,0,0,0,0,0,0]; //sunday is at index 0
+      var visited = [];
+      
+      for(var i = 0;i < $scope.task.progress.length; i++) { 
+        var progressDate = $scope.task.progress[i].date;
+        var day = progressDate.getDay();
+        var completeDate = Number(progressDate.getMonth().toString() + progressDate.getDate().toString()); 
+        //Check if this date is already visited
+        if(!(visited.indexOf(completeDate) > -1)) { 
+          visited.push(completeDate);
+          daysProgress[day] += 1; //+1 since there is a progress for this day
+        }
+        console.log("for day " + day + ", daysProgress is: " + daysProgress[day]);
+      }
+      var d = daysProgress.indexOf(Math.max.apply(Math,daysProgress));
+      var mostFrequentDay = daysOfWeek[d];
+      console.log("most frequent day is: " + mostFrequentDay);
+      return mostFrequentDay;
+    }
+  };
+  
+  
+  /**
+   * Return true if the task is weekly or monthly
+   */
+  $scope.isWeeklyOrMonthly = function() {
+    return $scope.task.frequency == 1 || $scope.task.frequency == 2;
+  }
+  
+  /**
    * Return back to the previous page
    */
   $scope.goBack = function() {
